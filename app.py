25388451c9a738255028a43a6525468a67c01ba8
Gordon Ball <gball261@gmail.com>
2022-10-13 15:29:35 -0400

    built question page

--- app.py
+++ app.py
@@ -7,3 +7,36 @@ app.config['SECRET_KEY'] = "never-tell!"
 app.config['DEBUG_TB_INTERCEPT_REDIRECTS'] = False
 
 debug = DebugToolbarExtension(app)
+
+responses = []
+
+
+@app.get("/")
+def show_start_page():
+    """ Displays survey start page """
+
+    return render_template("survey_start.html",
+        title=survey.title,
+        instructions=survey.instructions)
+
+
+@app.post("/begin")
+def start_survey():
+    """ """
+
+    return render_template("question.html",
+        question=survey.questions[0])
+
+
+
+@app.get("/question/<question_id>")
+def generate_question(question_id):
+    """ """
+
+    return render_template("question.html",
+        question=survey.questions[question_id])
+
+
+@app.post("/answer")
+def answer_question():
+    """ """
